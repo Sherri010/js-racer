da017f905e0c7db230837b48c0f67833be683b47
sherri010 <aminshahidi.sh@gmail.com>
2016-09-10 16:01:26 -0700

    working on extra features

--- code/app.js
+++ code/app.js
@@ -1,5 +1,7 @@
 
 document.getElementsByTagName('body')[0].addEventListener("keydown",addMiles);
+document.getElementById('start').addEventListener('click',startTime);
+document.getElementById('timer').textContent="00:00";
 var player1 = document.getElementById('p1');
 var player2 = document.getElementById('p2');
 var p1IsAt = 0;
@@ -12,7 +14,7 @@ function addMiles(e){
 
 	//console.log("running")
 	if(e.keyCode == 38) {
-		p1IsAt+=10;
+		p1IsAt+=15;
 		console.log( p1IsAt);
 		player1.style.left=p1IsAt.toString()+"px";
 		//console.log("playerf1");
@@ -20,7 +22,7 @@ function addMiles(e){
 		scoreCheck();
 	}
 	if(e.keyCode == 40){
-		p2IsAt+=10;
+		p2IsAt+=15;
 		player2.style.left=p2IsAt.toString()+"px";
 		//console.log("player2")
 
@@ -30,12 +32,38 @@ function addMiles(e){
 }
 
 function scoreCheck(){
-	if(p1IsAt >= 1150) { alert("Red Laser Win!");}
-	if(p2IsAt >= 1150) { alert("Blue Laser Win!");}
+	if(p1IsAt >= 1350) { alert("Red Laser Win!");}
+	if(p2IsAt >= 1350) { alert("Blue Laser Win!");}
 }
 
 
+function startTime() {
+  document.getElementById('start').style.visibility="hidden";
+	var timer= document.getElementById('timer');
+	        var totalSeconds = 0;
+	        setInterval(setTime, 1000);
 
+	        function setTime()
+	        {
+	            ++totalSeconds;
+	        ;
+	          timer.innerHTML = pad(parseInt(totalSeconds/60))+" : "+   pad(totalSeconds%60);
+	        }
+
+
+}
+function pad(val)
+{
+		var valString = val + "";
+		if(valString.length < 2)
+		{
+				return "0" + valString;
+		}
+		else
+		{
+				return valString;
+		}
+}
 
 
 // //save scores
