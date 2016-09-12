cd1333c29bd753d3773b34b49d8564909fa141e3
sherri010 <aminshahidi.sh@gmail.com>
2016-09-11 20:41:24 -0700

    adding reset button

--- code/app.js
+++ code/app.js
@@ -1,8 +1,12 @@
 
-
+//reset game
+//add points
 document.getElementById('start').addEventListener('click',startTime);
+document.getElementById('reset').addEventListener('click',resetGame);
 document.getElementById('timer').textContent="00 : 00";
 document.getElementsByClassName('scores')[0];
+var announce=document.createElement('div');
+announce.setAttribute("class","announce");
 var player1 = document.getElementById('p1');
 var player2 = document.getElementById('p2');
 var p1IsAt = 0;
@@ -16,7 +20,7 @@ function addMiles(e){
 	//console.log("running")
 	if(e.keyCode == 38) {
 		p1IsAt+=15;
-		console.log( p1IsAt);
+	//	console.log( p1IsAt);
 		player1.style.left=p1IsAt.toString()+"px";
 		//console.log("playerf1");
 
@@ -41,14 +45,9 @@ function scoreCheck(){
 
 
 function announcer(winner){
-
+	document.getElementsByTagName('body')[0].removeEventListener("keydown",addMiles);
   var winning_time=document.getElementById('timer').textContent;
-	var announce=document.createElement('div');
-	announce.style.background="#fff";
-	announce.style.width="500px";
-	announce.style.margin="0 auto";
-	announce.style.zIndex="10";
-	announce.textContent= winner +" win in "+ winning_time ;
+	announce.textContent= winner +" Wins in "+ winning_time ;
   document.getElementById('playground').appendChild(announce);
 
 
@@ -57,50 +56,39 @@ function announcer(winner){
 
 
 
-
+  var totalSeconds = 0;
+	var timer= document.getElementById('timer');
 /// timer
 function startTime() {
   document.getElementById('start').style.visibility="hidden";
 	document.getElementsByTagName('body')[0].addEventListener("keydown",addMiles);
-	var timer= document.getElementById('timer');
-	        var totalSeconds = 0;
-	        setInterval(setTime, 1000);
-
-	        function setTime()
-	        {
-	            ++totalSeconds;
-	        ;
-	          timer.innerHTML = pad(parseInt(totalSeconds/60))+" : "+   pad(totalSeconds%60);
-	        }
-
+    controlTimer('run');
 
 }
-function pad(val)
+
+function setTime()
 {
-		var valString = val + "";
-		if(valString.length < 2)
-		{
-				return "0" + valString;
-		}
-		else
-		{
-				return valString;
-		}
+		++totalSeconds;
+	timer.innerHTML = pad(parseInt(totalSeconds/60))+" : "+   pad(totalSeconds%60);
+
+	function pad(val)
+	{
+			var valString = val + "";
+	  	return 	(valString.length < 2)?   "0" + valString : valString;
+
+	}
 }
 
 
-// //save scores
-// //add one point for every 5 press key
-// //winner should reach 100 points
+function controlTimer(flag){
+	(flag === "run")?   setInterval(setTime, 1000): clearInterval(setTime);
+}
 
-
-// //arrow keys are only triggered by onkeydown, not onkeypress
-// // left = 37
-// // up = 38
-// // right = 39
-// // down = 40
-
-//             alert('down');
-//             break;
-//     }
-// };
+function resetGame(){
+          document.getElementById('start').style.visibility="visible";
+          controlTimer("reset");
+					 document.getElementById('timer').innerHTML="00 : 00";
+					 	player1.style.left="0px";
+						player2.style.left="0px";
+					  document.getElementById('playground').removeChild(announce);
+}
